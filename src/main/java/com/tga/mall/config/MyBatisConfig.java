b7d270af506df138d0ddc1ae0714e2d72913bf78
tgaqep <1457091108@qq.com>
2020-01-14 14:21:29 +0800

    test

---
+++ src/main/java/com/tga/mall/config/MyBatisConfig.java
@@ -0,0 +1,11 @@
+package com.tga.mall.config;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.tga.mall.mbg.mapper")
+public class MyBatisConfig {
+
+}
---
+++ src/main/java/com/tga/mall/service/PmsBrandService.java
@@ -0,0 +1,20 @@
+package com.tga.mall.service;
+
+import com.tga.mall.mbg.model.PmsBrand;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface PmsBrandService {
+    List<PmsBrand> listAllBrand();
+
+    int createBrand(PmsBrand brand);
+
+    int updateBrand(Long id, PmsBrand brand);
+
+    int deleteBrand(Long id);
+
+    List<PmsBrand> listBrand(int pageNum, int pageSize);
+
+    PmsBrand getBrand(Long id);
+}
